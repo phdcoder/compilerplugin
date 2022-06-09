a46d29e3b8b39f96c7aaa7262a77f5a6e1d3a308
GitHub Action <action@github.com>
2022-06-09 20:53:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/phdcoder/compilerplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/phdcoder/compilerplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.phdcoder.compilerplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phdcoder.compilerplugin.MyBundle
 
 class MyApplicationService {
 
